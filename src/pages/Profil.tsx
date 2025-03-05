3dcc48beffa135cbe9d16d99402b439ef165c94b
Laura Bensimon <lbe@ucaya.com>
2025-03-05 16:28:12 +0100

    change style

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -10,7 +10,7 @@ export const Profil: React.FC = () => {
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', gap: '50px', width: '65vw', backgroundColor: '#a3b4ff2a', borderRadius: '10px', padding: '25px' }}>
         <div style={{ width: '20%', height: '20%', display: 'flex', paddingTop: '40px' }}>
-          <img style={{ width: '100%', borderRadius: '99%', border: '1px solid #747BFF' }} src="https://external-preview.redd.it/looking-for-cat-holding-camera-meme-v0-hHXl1YNEuBrwzSTG72BYhBxhJi30D8stnUQccN6hMP0.jpg?auto=webp&s=d3db9cf4e90d3e6904bca67f752107177082086c" alt="" />
+          <img style={{ width: '100%', borderRadius: '99%', border: '10px solid #E8F0FE' }} src="https://external-preview.redd.it/looking-for-cat-holding-camera-meme-v0-hHXl1YNEuBrwzSTG72BYhBxhJi30D8stnUQccN6hMP0.jpg?auto=webp&s=d3db9cf4e90d3e6904bca67f752107177082086c" alt="" />
         </div>
         <div style={{ width: '55%', display: 'flex', flexDirection: 'column' }}>
           <Input textKey="Nom" type="text" name="name" value="" isDisabled={true} />
